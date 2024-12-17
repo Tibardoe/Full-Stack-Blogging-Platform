75a455443bab580d9c8ba708ca89e2a3383c36ec
Tibardoe <tibardoebenjamin@gmail.com>
2024-12-17 23:11:57 +0000

    CORS config

--- server/server.js
+++ server/server.js
@@ -55,9 +55,9 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         maxAge: 24 * 60 * 60 * 1000,
-        httpOnly: false,
-        secure: false,
-        sameSite: "Lax"
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
 
     }
 }));
