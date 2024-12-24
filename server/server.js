cb9f2f70b659d4797a008172b11f756dde361ab9
Tibardoe <tibardoebenjamin@gmail.com>
2024-12-24 10:54:31 +0000

    CORS and session config

--- server/server.js
+++ server/server.js
@@ -56,10 +56,9 @@ app.use(session({
     cookie: {
         domain: process.env.NODE_ENV === "production" ? "freeedom.netlify.app" : undefined,
         maxAge: 24 * 60 * 60 * 1000, // 1 day
-        httpOnly: false,
+        httpOnly: true,
         secure: false,
-        sameSite: "lax"
-        // httpOnly: true,
+        sameSite: "lax",
         // secure: process.env.NODE_ENV === "production", // Set `true` in production
         // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax" // "none" for cross-origin
     }
